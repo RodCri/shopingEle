6ffb4d9be2f9c208eb600fab1f7750050925806f
RodCri <cristian.rodriguez1317@gmail.com>
2024-06-23 11:48:53 -0500

    add Navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import {MyOrder} from '../MyOrder'
 import {MyOrders} from '../MyOrders'
 import {NotFound} from '../NotFound'
 import {SignIn} from '../SignIn'
+import { Navbar } from '../../Components/NavBar'
 
 const AppRoutes = () =>{
   let routes = useRoutes([
@@ -22,6 +23,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes/>
+      <Navbar/>
     </BrowserRouter>
   )
 }
